664b400186535e0eb828d7baa2f964a24dc26b8e
Jesuscastroti <jesusdavidcastroolaya@gmail.com>
2020-07-26 13:03:41 -0500

    Creando API

--- public/javascripts/Maps.js
+++ public/javascripts/Maps.js
@@ -1,9 +1,9 @@
-var mymap = L.map('mapid').setView([10.975152,-74.829397], 16);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+var map = L.map('mapid').setView([10.9751726, -74-8307983], 18);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([10.974957, -74.829961]).addTo(map)
+    .bindPopup('El barrio donde vivo')
+    .openPopup();
